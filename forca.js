d42582b6b3510c39273cced36adac0c07c300d43
João Victor <victo_phone@hotmail.com>
2022-03-03 23:05:44 -0300

    QUASE CABO

--- forca.js
+++ forca.js
@@ -11,13 +11,24 @@ let tema;
 let palavraSorteada;
 let palavraSecreta = [];
 let palavraResultado = [];
+let palavraLimpa;
 let underline;
-let errou;
+let errou = [];
 let erros = [];
 let vida = 7;
 let input;
-let vitoria;
-let derrota;
+let vitoria = 0;
+let derrota = 0;
+let desabilita;
+const teclado = document.getElementById('teclado_virtual')
+
+let evento = document.getElementById('submit-texto');
+
+
+let reiniciar = document.querySelector("#reiniciar")
+reiniciar.addEventListener("click", function () {
+    location.reload();
+});
 
 function getTema () {
     tema = document.getElementById("select-temas").value;
@@ -26,6 +37,13 @@ function getTema () {
 
 function selecionaPalavra(arrayPalavras) {
     getTema();
+    erros = [];
+    errou.innerHTML = erros;
+    vida = 7;
+    vidasForca(vida);
+    
+
+    teclado.style.display = 'block';
     const indicePalavra = parseInt(Math.random() * arrayPalavras[tema].length);
     palavraSorteada = arrayPalavras[tema][indicePalavra];
     palavraSorteada = palavraSorteada.toUpperCase();
@@ -43,6 +61,7 @@ function defineForca(palavraSorteada) {
 
 function recebeLetra (letra) {
     let posicaoLetra = palavraSecreta.indexOf(letra);
+    verificaLetraEscolhida(letra);
 
     if (palavraResultado.includes(letra)) {
         alert('Letra ja descoberta');
@@ -52,13 +71,19 @@ function recebeLetra (letra) {
         return;
     }
 
+    if (vida == 0) {
+        computaDerrota();
+    } else if (palavraLimpa.indexOf('_') === -1) {
+        computaVitoria();
+    }
+
     if (posicaoLetra == -1) {
-        errou = document.getElementById('letras-erradas');
+        errou = document.getElementById("letras-erradas");
         erros.push(letra);
         errou.innerHTML = erros.join(', ');
         vidasForca(--vida)
-        return;
     }
+
     while (posicaoLetra != -1) {
         palavraResultado[posicaoLetra] = letra;
         posicaoLetra = palavraSecreta.indexOf(letra, posicaoLetra + 1);
@@ -68,13 +93,71 @@ function recebeLetra (letra) {
         console.log(palavraResultado);
         console.log(palavraSecreta);
     }
+
+    if (vida == 0) {
+        computaDerrota();
+    } else if (palavraLimpa.indexOf('_') === -1) {
+        computaVitoria();
+    }
     return;
 }
 
+function verificaLetraEscolhida(letra) {
+    desabilita = document.getElementById("button-" + letra.toLowerCase()).disabled = true;
+}
+
+function palpite(event) {
+    event.preventDefault();
+    let palpitePalavra = document.getElementById("palpite-texto");
+    palpitePalavra = palpitePalavra.value;
+    palpitePalavra = palpitePalavra.toUpperCase();
+    palpitePalavra = palpitePalavra.split('');
+    let palpitecerto;
+    if (palpitePalavra.length == palavraSecreta.length){
+        for (i=0; i <= palpitePalavra.length; i++) {
+            if (palpitePalavra[i] == palavraSecreta[i]) {
+                palpitecerto = true;
+            }
+            else {
+                palpitecerto = false;
+            }
+        }
+    } else {
+        palpitecerto = false;
+    }
+    if (palpitecerto == false) {
+        ativaInput();
+        return computaDerrota();
+    } else {
+        underline.innerHTML = palavraSorteada;
+        ativaInput();
+        return computaVitoria();
+    }
+    return;
+}
+
+function ativaInput () {
+    const botao = document.getElementById("palpite-sim-button");
+    const palpite = document.getElementById("palpite-texto");
+    const botaoPalpite = document.getElementById("submit-texto");
+    
+    let palpitas = palpite.style.display;
+    if (palpitas == "none") {
+
+        palpite.style.display = "block";
+        botaoPalpite.style.display = "block";
+    } else {
+        palpite.style.display = "none";
+        botaoPalpite.style.display = "none";
+    }
+}
 
 function vidasForca (vida) {
  
     switch (vida) {
+        case 7:
+            document.getElementById('forca-img').src="./assets/images/Forca 0.png";
+            break;
         case 6:
             document.getElementById('forca-img').src="./assets/images/Forca 1.png";
             break;
@@ -97,28 +180,26 @@ function vidasForca (vida) {
             document.getElementById('forca-img').src="./assets/images/Forca 7.png";
             break;
     }
-
-    if ( vida == 0) {
-        alert('Perdeste');
-        derrota = document.getElementById('derrotas').innerHTML;
-        derrota++;
-        //vida = 7;
-        vidasForca(vida);
-        erros = [];
-        errou.innerHTML = erros;
-        return selecionaPalavra(arrayPalavras), derrota;
-    }
 }
 
 function computaVitoria() {
-    if (palavraLimpa.indexOf('_') === -1) {
-        alert('Ganhaste');
-        vitoria = document.getElementById('vitorias').innerHTML;
-        vitoria++;
-        vidasForca(vida);
-        console.log(vitoria);
-        return palavraSorteada, vitoria;
-    }
+    vitoriaHTML = document.getElementById("vitorias");
+    vitoria++;
+    vitoriaHTML.innerHTML = vitoria;
+    vidasForca(vida);
+    teclado.style.display = 'none';
+    desabilia = false;
+    return alert('Ganhaste');
+}
+
+function computaDerrota() {
+    derrotaHTML = document.getElementById('derrotas');
+    derrota++;
+    derrotaHTML.innerHTML = derrota;
+    vidasForca(vida);
+    teclado.style.display = "none";
+    
+    return alert('Perdeste troxão, a palavra era: ' + palavraSorteada);
 }
 
 /*
@@ -130,17 +211,20 @@ function derrota(palavra) {
 }*/
 
 document.onkeyup = (event) => {
-    if (event.key === "Enter") {
+    const foco = document.getElementById("palpite-texto")
+    if ( foco === document.activeElement) {
+        return;
+    } else if (event.key === "Enter") {
         selecionaPalavra(arrayPalavras);
         return;
     } else if (event.keyCode == 186) {
         input = event.key;
         recebeLetra(input.toUpperCase());
         return;
-    }
-    if (event.keyCode > 90 || event.keyCode < 65) {
+    } else if (event.keyCode > 90 || event.keyCode < 65) {
         return;
     }
+
     input = event.key;
     recebeLetra(input.toUpperCase());
 }
